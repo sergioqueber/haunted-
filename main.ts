db615e93c530ecac43566cf1e8f0bc0c8eb30c58
Samu1311 <91956612+Samu1311@users.noreply.github.com>
2021-10-25 20:46:43 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-let velocity = 0
 let list = [-50, 50]
 scene.setBackgroundImage(img`
     8888888888888888888888888888888888
@@ -25,6 +24,7 @@ scene.setBackgroundImage(img`
 tiles.setTilemap(tilemap`level 1`)
 info.setLife(2)
 let mySprite = sprites.create(assets.image`Teacher right`, SpriteKind.Player)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(6, 13))
 scene.cameraFollowSprite(mySprite)
 let Enemy_1 = sprites.create(assets.image`Enemy 1 left`, SpriteKind.Enemy)
 tiles.placeOnTile(Enemy_1, tiles.getTileLocation(6, 13))
@@ -32,6 +32,7 @@ Enemy_1.setBounceOnWall(false)
 let Enemy_2 = sprites.create(assets.image`Enemy 2 left`, SpriteKind.Enemy)
 tiles.placeOnTile(Enemy_2, tiles.getTileLocation(22, 17))
 Enemy_2.setBounceOnWall(false)
+tiles.placeOnTile(Enemy_2, tiles.getTileLocation(22, 17))
 let Enemy_3 = sprites.create(assets.image`Enemy 3 left`, SpriteKind.Enemy)
 tiles.placeOnTile(Enemy_3, tiles.getTileLocation(24, 13))
 Enemy_3.setBounceOnWall(false)
@@ -42,9 +43,23 @@ let Enemy_5 = sprites.create(assets.image`Enemy 5 left`, SpriteKind.Enemy)
 tiles.placeOnTile(Enemy_5, tiles.getTileLocation(17, 5))
 Enemy_5.setBounceOnWall(false)
 forever(function () {
-    velocity = list._pickRandom()
-    pause(1000)
+    controller.moveSprite(mySprite)
 })
 forever(function () {
-	
+    if (0 < 0) {
+    	
+    }
+})
+forever(function () {
+    pause(2000)
+    Enemy_1.setVelocity(0, 50)
+    Enemy_1.setVelocity(list._pickRandom(), 50)
+    Enemy_2.setVelocity(0, 50)
+    Enemy_2.setVelocity(list._pickRandom(), 50)
+    Enemy_3.setVelocity(0, 50)
+    Enemy_3.setVelocity(list._pickRandom(), 50)
+    Enemy_4.setVelocity(0, 50)
+    Enemy_4.setVelocity(list._pickRandom(), 50)
+    Enemy_5.setVelocity(0, 50)
+    Enemy_5.setVelocity(list._pickRandom(), 50)
 })
